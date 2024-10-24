4dc0852dd9d8b38e23a621e5ed9a7c158dbf9476
Luis Fernando Lee <102424560+luisferleerod@users.noreply.github.com>
2024-10-23 20:01:14 -0500

    Componente landing principal
    Apenas se ejecuta angular, carga el componente landing

--- front-clustero/src/app/app.component.ts
+++ front-clustero/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, LandingComponent],
+  template: `<app-landing></app-landing>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
